a3723690b9afff95ef46203b466b6415e5ad39ca
anushka-u <anushkau032@gmail.com>
2023-09-23 17:00:23 +0530

    Comments in StringInJava -> String Demonstration

--- StringInJava/src/string/In/Java/StringDemonstration.java
+++ StringInJava/src/string/In/Java/StringDemonstration.java
@@ -4,6 +4,8 @@ public class StringDemonstration {
 	
 	public static void main(String[] args) {
 		// Here What does happens is when we try to manipulate the value of string, it does create another string in our scp and start pointing to it without changing the value of s.
+		//String are immutable in Java, So if you are declaring a string whose value can change in near future do go with StringBuilder or StringBuffer
+		//Use String data type only when you are declaring a string whose value will not be changing in future
 		
 		String s = "Navin";
 		s = s+" Reddy";
