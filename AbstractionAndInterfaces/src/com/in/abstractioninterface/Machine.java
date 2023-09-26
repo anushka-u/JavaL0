b916a654090663e443380bac5fe7d38d3d05f667
anushka-u <anushkau032@gmail.com>
2023-09-26 23:09:18 +0530

    Java OOPS Concept

--- AbstractionAndInterfaces/src/com/in/abstractioninterface/Machine.java
+++ AbstractionAndInterfaces/src/com/in/abstractioninterface/Machine.java
@@ -1,5 +1,37 @@
 package com.in.abstractioninterface;
 
-public class Machine {
+abstract class DieselMachine
+{
+	public DieselMachine()
+	{
+		System.out.println("Diesel MAchine :: Constructor");
+	}
+	
+	public void start()
+	{
+		System.out.println("Machine Starting ....");
+	}
+	
+	public abstract void fillFuel();
+	
+}
+public class Machine extends DieselMachine {
 
+	@Override
+	public void fillFuel() {
+		// TODO Auto-generated method stub
+		System.out.println("Filling fuel tank ....");
+		
+	}
+	
+	public static void main(String... args)
+	{
+		// Abstract class contains constructor but still object for abstract classes cannot be created
+		// To create an object of abstract class, a subclass is required which does implement the abstract class.
+		
+
+	}
+	
+	
+	
 }
