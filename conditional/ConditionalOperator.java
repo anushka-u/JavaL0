0c182d20516a277f5753af8a626e13a1d398f14a
anushka-u <anushkau032@gmail.com>
2024-02-04 11:45:51 +0530

    Java Rev

---
+++ conditional/ConditionalOperator.java
@@ -0,0 +1,19 @@
+package com.in.conditional;
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		a = 12;
+		
+		b = (a==12)?30:40;
+		System.out.println(b);
+		
+		b=(a==1)?23:78;
+		System.out.println(b);
+
+	}
+
+}
